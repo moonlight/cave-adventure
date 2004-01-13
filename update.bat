a283569135024ede4eedc31238220ed7638f0a04
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2004-01-13 19:43:12 +0000

    Misc stuff

---
+++ update.bat
@@ -0,0 +1,6 @@
+del data.dat
+dat -a -k -t LUA  data.dat data\scripts\*.lua
+dat -a -k -t BMP  data.dat data\bitmaps\*.bmp data/bitmaps\*.tga
+dat -a -k -t FONT data.dat data\bitmaps\font_*.*
+dat -a -k -t MAP  data.dat data\maps\*.map
+dat -a -k -t SAMP data.dat data\samples\*.wav
